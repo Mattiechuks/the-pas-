4e75617a33b3a136dc41810484c5ff1292095f7f
Chukwuji Matthew Olisenekwu <73289923+Mattiechuks@users.noreply.github.com>
2024-01-20 18:39:05 +0100

    Update 2_Registration_form.py

--- pages/2_Registration_form.py
+++ pages/2_Registration_form.py
@@ -35,7 +35,6 @@ def video_callback_func(self, frame):
 webrtc_streamer(key='registration',video_frame_callback=video_callback_func,
                rtc_configuration=RTCConfiguration(
                  {"iceServers": [{"urls": [
-                                            "stun2.l.google.com:19302",
                                             "stun3.l.google.com:19302",
                                             "stun4.l.google.com:19302",
                                             "stun.ekiga.net",
